3dfe147ae8ab5bfafdd702fad807ca5017d153cc
Mitchel Bower <mitchelbower@outlook.com>
2024-08-19 15:50:43 -0400

    update

--- src/App.js
+++ src/App.js
@@ -11,19 +11,24 @@ export default function App() {
 function SetBudgetForm() {
   const [budget, setBudget] = useState("");
   const [haveSpent, setHaveSpent] = useState("");
-  const remainder = budget - haveSpent;
+  const [remainder, setRemainder] = useState("");
+
+  function handleSetRemainder(e) {
+    e.preventDefault();
+    setRemainder(budget - haveSpent);
+  }
 
   return (
     <div className="set-budget-form">
       <form className="form">
-        <label>Set a budget</label>
+        <label>Set budget</label>
         <input
           className={haveSpent > budget ? "negative" : ""}
           type="text"
           value={budget}
           onChange={(e) => setBudget(Number(e.target.value))}
         />
-        <button>Set</button>
+        <button onClick={handleSetRemainder}>Set</button>
         <label>How much have you spent?</label>
         <input
           type="text"
