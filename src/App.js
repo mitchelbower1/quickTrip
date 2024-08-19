0c50ac85c56a25bc4f0eb8fe24003f60dd2dd219
Mitchel Bower <mitchelbower@outlook.com>
2024-08-19 10:30:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <SetBudgetForm />
+    </div>
+  );
+}
+
+function SetBudgetForm() {
+  const [budget, setBudget] = useState("");
+  const [haveSpent, setHaveSpent] = useState("");
+  const remainder = budget - haveSpent;
+
+  return (
+    <div className="set-budget-form">
+      <form className="form">
+        <label>Set a budget</label>
+        <input
+          type="text"
+          value={budget}
+          onChange={(e) => setBudget(Number(e.target.value))}
+        />
+        <button>Set</button>
+        <label>How much have you spent?</label>
+        <input
+          type="text"
+          value={haveSpent}
+          onChange={(e) => setHaveSpent(Number(e.target.value))}
+        />
+        <label>remainder</label>
+        <input type="text" disabled value={remainder} />
+      </form>
+    </div>
+  );
+}
